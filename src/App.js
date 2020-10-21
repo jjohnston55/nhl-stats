866a38129a3476165c340af47d22bc7c25abe336
jjohnston55 <46493038+jjohnston55@users.noreply.github.com>
2020-10-20 18:37:29 -0600

    Completed functional home page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Home from './components/pages/home';
 import Team from './components/pages/team';
 import Player from './components/pages/player';
 import './App.css';
+import 'antd/dist/antd.css';
 
 
 const App = () => {
 	return (
-		<>
+		<div className='content'>
 			<Home />
 			<Team />
 			<Player />
-		</>
+		</div>
 	)
 }
 
--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,27 +1,122 @@
-import React, { useEffect } from 'react';
-import { Row, Col } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Row, Col, Button, Alert } from 'antd';
+import './index.css';
+
 import NHL from '../../../api';
 
 const Home = (props) => {
     const api = new NHL();
+    const [pacificTeams, setPacificTeams] = useState([]);
+    const [centralTeams, setCentralTeams] = useState([]);
+    const [metroTeams, setMetroTeams] = useState([]);
+    const [atlanticTeams, setAtlanticTeams] = useState([]);
     
     useEffect(() => {
         api.GetTeams().then(data => {
-            console.log(data.teams);
+            const pTeams = []
+            const cTeams = [];
+            const mTeams = [];
+            const aTeams = [];
+            data.teams.forEach(team => {
+                switch (team.division.abbreviation) {
+                    case 'P':
+                        pTeams.push(team);
+                        break;
+                    case 'C':
+                        cTeams.push(team);
+                        break;
+                    case 'M':
+                        mTeams.push(team);
+                        break;
+                    case 'A':
+                        aTeams.push(team);
+                        break;
+                }
+            });
+            setPacificTeams(pTeams);
+            setCentralTeams(cTeams);
+            setMetroTeams(mTeams);
+            setAtlanticTeams(aTeams);
         });
     }, []);
 
     return (
         <>
-            <Row>
-                <Col span={12}>Western Conference</Col>
-                <Col span={12}>Eastern Conference</Col>
+            <Row gutter={[16,16]}>
+                <Col span={12}>
+                    <Alert className='centerText' message='Western Conference' type='info' />
+                </Col>
+                <Col span={12}>
+                    <Alert className='centerText' message='Eastern Conference' type='info' />
+                </Col>
             </Row>
-            <Row>
-                <Col>Pacific</Col>
-                <Col>Central</Col>
-                <Col>Metropolitan</Col>
-                <Col>Atlantic</Col>
+            <Row gutter={[16,16]}>
+                <Col span={6}>
+                    <Alert className='centerText' message='Pacific' type='info' />
+                </Col>
+                <Col span={6}>
+                    <Alert className='centerText' message='Central' type='info' />
+                </Col>
+                <Col span={6}>
+                    <Alert className='centerText' message='Metropolitan' type='info' />
+                </Col>
+                <Col span={6}>
+                    <Alert className='centerText' message='Atlantic' type='info' />
+                </Col>
+            </Row>
+            <Row gutter={[16, 16]}>
+                <Col span={6}>
+                    {
+                        pacificTeams.map((team, idx) => {
+                            return (
+                                <Row key={idx} gutter={[16, 16]}>
+                                    <Col span={24}>
+                                        <Button onClick={() => console.log(team)} type='primary' size='large' block>{team.teamName}</Button>
+                                    </Col>
+                                </Row>
+                            )
+                        })
+                    }
+                </Col>
+                <Col span={6}>
+                    {
+                        centralTeams.map((team, idx) => {
+                            return (
+                                <Row key={idx} gutter={[16, 16]}>
+                                    <Col span={24}>
+                                        <Button onClick={() => console.log(team)} type='primary' size='large' block>{team.teamName}</Button>
+                                    </Col>
+                                </Row>
+                            )
+                        })
+                    }
+                </Col>
+                <Col span={6}>
+                    {
+                        metroTeams.map((team, idx) => {
+                            return (
+                                <Row key={idx} gutter={[16, 16]}>
+                                    <Col span={24}>
+                                        <Button onClick={() => console.log(team)} type='primary' size='large' block>{team.teamName}</Button>
+                                    </Col>
+                                </Row>
+                            )
+                        })
+                    }
+                </Col>
+                <Col span={6}>
+                    {
+                        atlanticTeams.map((team, idx) => {
+                            return (
+                                <Row key={idx} gutter={[16, 16]}>
+                                    <Col span={24}>
+                                        <Button onClick={() => console.log(team)} type='primary' size='large' block>{team.teamName}</Button>
+                                    </Col>
+                                </Row>
+                            )
+                        })
+                    }
+                </Col>
             </Row>
         </>
     )
