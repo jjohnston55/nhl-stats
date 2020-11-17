91675ce9a7a90d107ed5c0cacb0aef2a2ab252e4
jjohnston55 <46493038+jjohnston55@users.noreply.github.com>
2020-11-16 22:04:35 -0700

    Updates

---
+++ precache-manifest.8ad1b91c543bcce2e853cd3fcd042aa0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c72e18dbbaecede359673e79732a2e",
+    "url": "/nhl-stats/index.html"
+  },
+  {
+    "revision": "78d837adbcf514be5c33",
+    "url": "/nhl-stats/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "e9777c19870887474997",
+    "url": "/nhl-stats/static/css/main.08ecd228.chunk.css"
+  },
+  {
+    "revision": "78d837adbcf514be5c33",
+    "url": "/nhl-stats/static/js/2.e8e5c18d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/nhl-stats/static/js/2.e8e5c18d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9777c19870887474997",
+    "url": "/nhl-stats/static/js/main.d444a71b.chunk.js"
+  },
+  {
+    "revision": "6ae43bde9e11b95c6206",
+    "url": "/nhl-stats/static/js/runtime-main.e0804171.js"
+  },
+  {
+    "revision": "a68b5158cd524fa54d09f674d82c4494",
+    "url": "/nhl-stats/static/media/ice.a68b5158.svg"
+  }
+]);
