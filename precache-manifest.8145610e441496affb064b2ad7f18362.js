3a90b9b07d085e53e2009642d3bc40b5399f5157
jjohnston55 <46493038+jjohnston55@users.noreply.github.com>
2020-11-17 14:11:28 -0700

    Updates

---
+++ precache-manifest.8145610e441496affb064b2ad7f18362.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1715322100efa280bc7234c91ac0210",
+    "url": "/nhl-stats/index.html"
+  },
+  {
+    "revision": "9dce5a621aebc0913203",
+    "url": "/nhl-stats/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "beb31a655f221e92965d",
+    "url": "/nhl-stats/static/css/main.08e48030.chunk.css"
+  },
+  {
+    "revision": "9dce5a621aebc0913203",
+    "url": "/nhl-stats/static/js/2.a638bdc7.chunk.js"
+  },
+  {
+    "revision": "05235986e2a1f962e60754581aae269a",
+    "url": "/nhl-stats/static/js/2.a638bdc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb31a655f221e92965d",
+    "url": "/nhl-stats/static/js/main.bcd7044a.chunk.js"
+  },
+  {
+    "revision": "6ae43bde9e11b95c6206",
+    "url": "/nhl-stats/static/js/runtime-main.e0804171.js"
+  },
+  {
+    "revision": "19eadd561c4161e5d6196eafc25c04f7",
+    "url": "/nhl-stats/static/media/ice-90.19eadd56.svg"
+  },
+  {
+    "revision": "a68b5158cd524fa54d09f674d82c4494",
+    "url": "/nhl-stats/static/media/ice.a68b5158.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nhl-stats/precache-manifest.8ad1b91c543bcce2e853cd3fcd042aa0.js"
+  "/nhl-stats/precache-manifest.8145610e441496affb064b2ad7f18362.js"
 );
 
 self.addEventListener('message', (event) => {
